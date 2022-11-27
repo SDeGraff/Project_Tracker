c4b9cd7b7ed3c8f6ab9b4c53a40af1b87fc3df93
SDeGraff <114950127+SDeGraff@users.noreply.github.com>
2022-11-27 08:06:36 -0600

    Jquery don't work?

--- script.js
+++ script.js
@@ -13,23 +13,34 @@ $(document).ready(function () {
   $(".saveBtn").on("click", function(){
     var text = $(this).siblings(".description").val();
     var time = $(this).parent().attr('id');
-    localStorage.setItem
-  })
+    localStorage.setItem(time, text);
+  });
 
-  
     // TODO: Add code to apply the past, present, or future class to each time
     // block by comparing the id to the current hour. HINTS: How can the id
     // attribute of each time-block be used to conditionally add or remove the
     // past, present, and future classes? How can Day.js be used to get the
     // current hour in 24-hour time?
+
+  
     //
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
-    //
+
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour1 .description").val(localStorage.getItem("hour1"));
+    $("#hour2 .description").val(localStorage.getItem("hour2"));
+    $("#hour3 .description").val(localStorage.getItem("hour3"));
+    $("#hour4.description").val(localStorage.getItem("hour4"));
+    $("#hour5 .description").val(localStorage.getItem("hour5"));
+    
     // TODO: Add code to display the current date in the header of the page.
 
-    var now = dayjs().format('MMMM, D, YYYY');
+    var now = dayjs().format('MMMM, D, YYYY, h:mm');
     $('#currentDay').text(now)
     
   });
