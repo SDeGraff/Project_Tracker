de0b8b0983085e0e80e7ea10d05142b1826aad75
SDeGraff <114950127+SDeGraff@users.noreply.github.com>
2022-11-28 08:08:48 -0600

    fixed issue with ID name in getItem hour

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ $(document).ready(function () {
     var text = $(this).siblings(".description").val();
     var time = $(this).parent().attr('id');
     localStorage.setItem(time, text);
-  });
+  })
 
     // TODO: Add code to apply the past, present, or future class to each time
     // block by comparing the id to the current hour. HINTS: How can the id
@@ -25,29 +25,45 @@ $(document).ready(function () {
       var timeNow = dayjs().hour();
 
       $(".time-block").each(function() {
-        var blockTime = parseint($(this).attr("id").split("hour")[1]);
+        var blockTime = parseInt($(this).attr("id"));
 
-
+        if (blockTime < timeNow) {
+          $(this).removeClass("future");
+          $(this).removeClass("present");
+          $(this).addClass("past");
+        }
+        else if (blockTime === timeNow) {
+          $(this).removeClass("past");
+          $(this).removeClass("future");
+          $(this).addClass("present");
+        }
+        else {
+          $(this).removeClass("past");
+          $(this).removeClass("present");
+          $(this).addClass("future");
+        }
+        console.log(timeTracker);
       })
     }  
-    //
+   
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
 
-    $("#hour9 .description").val(localStorage.getItem("hour9"));
-    $("#hour10 .description").val(localStorage.getItem("hour10"));
-    $("#hour11 .description").val(localStorage.getItem("hour11"));
-    $("#hour12 .description").val(localStorage.getItem("hour12"));
-    $("#hour13 .description").val(localStorage.getItem("hour13"));
-    $("#hour14 .description").val(localStorage.getItem("hour14"));
-    $("#hour15 .description").val(localStorage.getItem("hour15"));
-    $("#hour16 .description").val(localStorage.getItem("hour16"));
-    $("#hour17 .description").val(localStorage.getItem("hour17"));
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
     
     // TODO: Add code to display the current date in the header of the page.
 
     var now = dayjs().format('MMMM, D, YYYY, h:mm:ss');
     $('#currentDay').text(now)
     
-  });
+    timeTracker();
+  })
